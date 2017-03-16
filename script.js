5e101caa85d7f8dc40a06d2763dab0b73618d497
kejsper\Programowanie <kejsper@gmail.com>
2017-03-16 20:43:53 +0100

    1. fixed menu links 2. added internal links animation 3. created basic structure of the website

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ $(document).ready(function () {
     setTimeout(adjustSlider, 100);
   });
 
+  // Inside links animation
+  $('a[href^="#"]').on('click', function (event) {
+    var target = $($(this).attr('href'));
+    console.log(target);
+    if ( target.length ) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: target.offset().top}, 700);
+    }
+  });
+
   scrolltop.on('click', function (event) {
     event.preventDefault();
     $('html, body').animate({scrollTop: 0}, 600);
